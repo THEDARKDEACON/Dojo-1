29f80a0bebad8e444c04c52271c617357799512f
thedarkdeacon <garethjoel77@gmail.com>
2025-09-18 10:19:15 +0200

    perception

--- src/robot_perception/setup.py
+++ src/robot_perception/setup.py
@@ -1,22 +1,26 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+from glob import glob
 
 package_name = 'robot_perception'
 
 setup(
     name=package_name,
     version='0.1.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/perception.launch.py']),
-        ('share/' + package_name + '/config', ['config/robot_perception_params.yaml']),
+        (os.path.join('share', package_name, 'launch'), 
+         glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), 
+         glob('config/*.yaml')),
     ],
-    install_requires=['setuptools'],
-    zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your.email@example.com',
+    install_requires=['setuptools', 'numpy', 'opencv-python>=4.5.0'],
+    zip_safe=False,
+    maintainer='robosync',
+    maintainer_email='robosync@example.com',
     description='Robot perception package for handling camera and vision processing',
     license='Apache License 2.0',
     tests_require=['pytest'],
